f53153a2c633d9a67f57df7858604f0d0b244c54
Kanako Miyoshi <miyoshi.344kanako1018@gmail.com>
2026-01-07 14:41:49 +0900

    コミットをたす

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 24 }}>
       <h1>tier-list-fixed</h1>
-      <p>例: /p/demo</p>
+      <p>例: /p/demoに移動してね</p>
     </main>
   );
 }
